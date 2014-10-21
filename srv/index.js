b805b85b2d9590a6f51e59d5ffaaa7a7c315ea21
rbr8 <bencsr@freemail.hu>
2014-10-21 17:09:29 +0000

    Separate sensor handling 2

--- srv/index.js
+++ srv/index.js
@@ -1,7 +1,6 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
-var path = require('path');
+var sensor = require('./sensor.js');
 
 var port = 2000;
 
@@ -25,35 +24,11 @@ http.createServer(function server(request, response) {
 		return;
 	}
 	
-	var result = readTemp(meterCode);
+	var meterId = sensors[meterCode];
+	var result = sensor.readTemp(meterId);
 	response.writeHead(200, { 'Content-Type': 'text/plain' });
 	response.end(result, 'utf-8');
 	
 }).listen(port);
-
-function readTemp(meterCode) {
-	var meterId = sensors[meterCode];
-
-	var lines = readTempRaw(meterId).split('\n');
-	
-	while (lines[0].slice(-3) !== 'YES') {
-		lines = readTempRaw(meterId).split('\n')
-	}
-	
-	return parseReading(meterId, lines);
-}
-
-function readTempRaw(meterId) {
-	var sourceFile = path.join('/sys/bus/w1/devices', meterId, 'w1_slave');
-	return fs.readFileSync(sourceFile, 'ascii');
-}
-
-function parseReading(meterId, lines) {
-	var origTemp = lines[1].split('t=')[1];
-	
-	var temp = parseInt(origTemp) / 1000.0;
-	
-	return temp.toFixed(1);
-}
 
 console.log('Server running at http://127.0.0.1:' + port + '/');

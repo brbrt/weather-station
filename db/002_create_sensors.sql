96672a3c20d440e8acfce3ed29cec9c66209c06e
rbr8 <bencsr@freemail.hu>
2014-10-26 13:48:01 +0100

    First DB scripts

---
+++ db/002_create_sensors.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS sensors;
+
+CREATE TABLE sensors (
+    sensor_id INT NOT NULL AUTO_INCREMENT,
+    code VARCHAR(256) NOT NULL,
+    type VARCHAR(256) NOT NULL,
+    physical_id VARCHAR(256) NOT NULL,
+    description VARCHAR(2048),
+    CONSTRAINT sensors_pk PRIMARY KEY (sensor_id)
+);

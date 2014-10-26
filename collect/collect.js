521b386c21b00a65374d31a485406c40c97c4c2d
rbr8 <bencsr@freemail.hu>
2014-10-26 21:06:59 +0100

    Improve collect

--- collect/collect.js
+++ collect/collect.js
@@ -1,40 +1,16 @@
 var $q = require('q');
+var log = require('winston');
 var spawn = require('child_process').spawn;
 
 var config = require('./config.js');
 
 
-var sensors = [
-	{
-		type: 'dht',
-		code: 'sm',
-		id: '15'
-	},
-	{
-		type: 'ds',
-		code: 'out',
-		id: '28-000005be30dc'
-	},
-	{
-		type: 'ds',
-		code: 'sm',
-		id: '28-000005815fdd'
-	},
-	{
-		type: 'ds',
-		code: 'lg',
-		id: '28-000005826997'
-	}
-];
+module.exports = collect;
 
 
-var result = {};
+function collect(sensors) {
+	log.debug('Start collecting sensor data.');
 
-
-readAll();
-
-
-function readAll() {
 	var prs = [];
 
 	for (var i = 0; i < sensors.length; i++ ) {
@@ -44,35 +20,49 @@ function readAll() {
 		prs.push(pr);
 	}
 
-	$q.all(prs).then(function success() {
-		console.log('All is done.');
-	});
+	return $q.all(prs);
 }
 
 function read(sen) {
+	var deferred = $q.defer();
+
+	log.debug('Read data of sensor: ' + JSON.stringify(sen));
+
 	var pr = (sen.type === 'dht') ? readDHT11(sen) : readDS18B20(sen);
 
 	pr.then(
-		function success(data) {
-			console.log(sen.type + ' ' + sen.code + '= ' + data);
+		function success(result) {
+			log.debug(sen.type + ' ' + sen.code + '= ' + data);
+
+			// Add the sensor info to the result.
+			deferred.resolve({
+				sensor: sen,
+				result: data
+			});
 		},
 		function error(err) {
-			console.log('Error:' + sen.type + ' ' + sen.code + '= ' + data);
+			log.debug('Error:' + sen.type + ' ' + sen.code + '= ' + data);
+			deferred.reject({
+				sensor: sen,
+				err: err
+			});
 		}
 	);
 
-	return pr;
+	return deferred.promise;
 }
 
 function readDHT11(sen) {
-	return callProc(config('sensors:dhtCommand'), [sen.id]);
+	return callProc(config('sensors:dhtCommand'), [sen.physical_id]);
 }
 
 function readDS18B20(sen) {
-	return callProc(config('sensors:dsCommand'), [sen.id]);
+	return callProc(config('sensors:dsCommand'), [sen.physical_id]);
 }
 
 function callProc(command, args) {
+	log.debug('callProc command=' + command + '  args=' + JSON.stringify(args));
+
 	var deferred = $q.defer();
 
 	var proc = spawn(command, args);
@@ -80,14 +70,18 @@ function callProc(command, args) {
 	var out = '', err = '';
 
 	proc.stdout.on('data', function onMsg(data) {
+		log.debug('callProc stdout: ' + data);
 	    out += data;
 	});
 
 	proc.stderr.on('data', function onErr(data) {
+		log.debug('callProc stderr: ' + data);
 	    err += data;
 	});
 
 	proc.on('close', function onClose(code) {
+		log.debug('callProc close: ' + code);
+
 		if (code === 0) {
 			deferred.resolve(out);
 		} else {
--- collect/index.js
+++ collect/index.js
@@ -1,13 +1,15 @@
 var config = require('./config.js');
 var log = require('winston');
 var getSensors = require('./db/get_sensors.js');
+var collect = require('./collect.js');
 
 log.debug('Start.');
-getSensors().then(
-    function success(data) {
-        log.info('Sensors: ', JSON.stringify(data));
-    },
-    function error(err) {
-        log.info('Error getting sensors: ', err);
-    }
-);
+getSensors()
+    .then(collect)
+    .then(handleResult)
+    .catch(log.error);
+
+
+function handleResult(res) {
+    console.log('Res:' + JSON.stringify(res));
+}

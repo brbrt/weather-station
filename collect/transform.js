a5e091df70a5d1694d1c57a4e861d6703bae1735
rbr8 <bencsr@freemail.hu>
2014-11-02 15:03:13 +0100

    Use local dates

--- collect/transform.js
+++ collect/transform.js
@@ -10,7 +10,7 @@ function transform(data) {
 
     var deferred = $q.defer();
 
-    var currDate = new Date().toISOString();
+    var currDate = getCurrDateTime();
 
     try {
         var transformed = [];
@@ -43,6 +43,29 @@ function transformItem(item, currDate) {
     };
 
     return tr;
+}
+
+function getCurrDateTime() {
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
 }
 
 function isNotNull(val) {

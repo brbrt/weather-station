f42ea81858e2add5c90594b46516ecfe370a9d52
rbr8 <bencsr@freemail.hu>
2014-12-25 12:04:00 +0100

    Add refresh button - forgotten files

--- fe/js/mainCtrl.js
+++ fe/js/mainCtrl.js
@@ -9,20 +9,32 @@ angular.module('weatherStation', [
 
 		$scope.sensorData = [];
 		$scope.measureDate = null;
+		$scope.isLoading = false;
+
+		$scope.refresh = getData;
 
 		init();
 
 		///////////////////
 
 		function init() {
+			getData();
+		}
+
+		function getData() {
+			$scope.isLoading = true;
+
 			actualSrv.getData().then(
 				function success(resp) {
+					$scope.isLoading = false;
+
 					var data = resp.data;
 					$scope.sensorData = data;
 
 					$scope.measureDate = data[0].date;
 				},
 				function error(err) {
+					$scope.isLoading = false;
 					alert('Hiba történt az adatok lekérdezése közben.');
 				}
 			);

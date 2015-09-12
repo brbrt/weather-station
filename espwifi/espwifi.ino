e7754e5846ff5bfb1be31b16c96bc92797ec51eb
brbrt <bencsr@freemail.hu>
2015-09-12 20:40:42 +0200

    Serial messages can be turned off

--- espwifi/espwifi.ino
+++ espwifi/espwifi.ino
@@ -11,13 +11,14 @@ float lastTemp = -15000;
 int readCount = 0;
 
 void setup() {
-  Serial.begin(115200);
-
-  delay(10);
+  if (DEBUG_ENABLED) {
+    Serial.begin(115200);
+    delay(100);
+  }
 
   initwifi();
 
-  Serial.println("Setup finished\n");
+  debug("Setup finished\n");
 }
 
 void loop() {
@@ -26,36 +27,33 @@ void loop() {
   float temp = readtemp();
   readCount++;
 
-  Serial.println("Temperature: " + String(temp) + ", readCount:" + String(readCount));
+  debug("Temperature: " + String(temp) + ", readCount:" + String(readCount));
 
   if (abs(temp - lastTemp) > VALUE_CHANGE_TOLERANCE || readCount == KEEPALIVE_ON_EVERY_X_READS) {
     lastTemp = temp;
     readCount = 0;
     sendData(temp);
   } else {
-    Serial.println("Not sending data now.");
+    debug("Not sending data now.");
   }
 
   unsigned long elapsed = millis() - start;
-  Serial.println("This loop took " + String(elapsed) + " millis.\n");
+  debug("This loop took " + String(elapsed) + " millis.\n");
 
   delay(READ_INTERVAL * 1000 - elapsed);
 }
 
 void initwifi() {
-  Serial.print("\n\nConnecting to ");
-  Serial.println(WIFI_SSID);
+  debug("\n\nConnecting to " + String(WIFI_SSID));
   
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    debug(".");
   }
 
-  Serial.print("\nWiFi connected. ");  
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  debug("\nWiFi connected. IP address: " + WiFi.localIP());  
 }
 
 float readtemp() {
@@ -71,11 +69,11 @@ float readtemp() {
     DS18B20.requestTemperatures(); 
     temp = DS18B20.getTempCByIndex(0);
 
-    Serial.print("Raw temperature: " + String(temp) + "  ");
+    debug("Raw temperature: " + String(temp) + "  ");
     retryCount++;
   } while (!isTemperatureValid(temp));
 
-  Serial.println();
+  debug("");
 
   return temp;
 }
@@ -85,16 +83,16 @@ bool isTemperatureValid(float temp) {
 }
 
 void sendData(float temp) {
-  Serial.println(String("Connecting to ") + TARGET_HOST + ":" + TARGET_PORT);
+  debug(String("Connecting to ") + TARGET_HOST + ":" + TARGET_PORT);
   
   WiFiClient client;
   if (!client.connect(TARGET_HOST, TARGET_PORT)) {
-    Serial.println("Connection failed");
+    debug("Connection failed");
     return;
   }
   
   String url = "/api/weather?sensor=" + String(SENSOR_ID) + "&temp=" + formatNumber(temp, VALUE_DECIMAL_PLACES);
-  Serial.println("Requesting URL: " + url);
+  debug("Requesting URL: " + url);
 
   client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                "Host: " + TARGET_HOST + "\r\n" + 
@@ -103,15 +101,21 @@ void sendData(float temp) {
   
   while (client.available()) {
     String line = client.readStringUntil('\r');
-    Serial.print(line);
+    debug(line);
   }
   
-  Serial.println();
+  debug("");
 }
 
 String formatNumber(float number, int decimalPlaces) {
   char charTemp[10];
   dtostrf(number, 1, decimalPlaces, charTemp);
   return String(charTemp);
+}
+
+void debug(String message) {
+  if (DEBUG_ENABLED) {
+    Serial.println(message);
+  }
 }
 
